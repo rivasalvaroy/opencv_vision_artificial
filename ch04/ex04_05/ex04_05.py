36d6ee8a8f826654b0c775c76e64fe2b3f2a2687
rivasalvaroy <rivasalvaroy@gmail.com>
2022-08-17 13:39:22 -0500

    ch04_005 - programa que dibuja circulos sobre una imagen con circle, pag 51

---
+++ ch04/ex04_05/ex04_05.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+def run():
+    img = cv2.imread('../../imagenes/cuadro.jpg', 1)
+
+    alto, ancho, canales = img.shape
+    color = (0, 0, 255)
+    incremento_radio = 80
+    grosor = 40
+
+    centro_x = int(ancho/2)
+    centro_y = int(alto/2)
+    for radio in range(0, int(alto/2)+1, incremento_radio):
+        cv2.circle(img, (centro_x, centro_y), radio, color, grosor)
+
+    cv2.imshow('cuadro', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    run()
